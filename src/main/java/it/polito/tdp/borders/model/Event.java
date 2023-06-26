55af2191d31d00194cecb3690bc99ac66f508c74
loren <loren@LAPTOP-0U9FS6EH.lan>
2023-06-26 15:48:43 +0200

    soluzione

---
+++ src/main/java/it/polito/tdp/borders/model/Event.java
@@ -0,0 +1,36 @@
+package it.polito.tdp.borders.model;
+
+public class Event implements Comparable<Event>{
+	private int time; // passi di simulazione
+	//tipo di evento: INGRESSO (sottointeso)
+	private Country destinazione;
+	private int dimensione;
+	public int getTime() {
+		return time;
+	}
+	
+	public Event(int time, Country destinazione, int dimensione) {
+		super();
+		this.time = time;
+		this.destinazione = destinazione;
+		this.dimensione = dimensione;
+	}
+
+	public Country getDestinazione() {
+		return destinazione;
+	}
+	public int getDimensione() {
+		return dimensione;
+	}
+
+	@Override
+	public String toString() {
+		return "Event [time=" + time + ", destinazione=" + destinazione + ", dimensione=" + dimensione + "]";
+	}
+
+	@Override
+	public int compareTo(Event other) {
+		return this.time-other.time;
+	}
+	
+}
